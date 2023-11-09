1db82efe4c972aaf2074e468ce0f05995c33d2f6
yesworld <6162264+yesworld@users.noreply.github.com>
2023-11-09 15:32:02 +0300

    chore(tutorial09): create api game and add confetti to game

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,30 @@ import { dataAnimals, dataBackground, soundsData } from './sources.ts'
 import ImageLoaderService from './services/ImageLoaderService.ts'
 import GameBuilder from './core/GameBuilder.ts'
 import AudioService from './services/AudioService.ts'
+import ConfettiService from './services/ConfettiService.ts'
 
-const gameBuilder = new GameBuilder(
-  new ImageLoaderService(),
-  new AudioService('/sound/'),
-  dataAnimals,
-)
+const audioService = new AudioService('/sound/')
+const gameBuilder = new GameBuilder(new ImageLoaderService(), audioService, dataAnimals)
 
 const game = await gameBuilder
   .loadSounds(soundsData)
   .loadBackground(dataBackground)
   .loadImageAnimals()
   .build()
+
+game.start()
+
+document.querySelector('#restart')?.addEventListener('click', () => {
+  game.restart()
+})
+
+document.querySelector('#mute')?.addEventListener('click', (e: MouseEvent) => {
+  const isMute = audioService.toggleSound()
+  const button = e.target as HTMLButtonElement
+  button.textContent = `Sound ${isMute ? '❌' : '🔉'}`
+})
+
+const confettiService = new ConfettiService()
+game.onEndGame(() => {
+  setTimeout(() => confettiService.start(3), 0)
+})
