84cd101ab89687d741ab98e5a09fe35c0c25df77
yesworld <6162264+yesworld@users.noreply.github.com>
2023-12-08 17:05:10 +0300

    build(ci/cd): prepare the project for build

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,32 @@ import ImageLoaderService from './services/ImageLoaderService.ts'
 import GameBuilder from './core/GameBuilder.ts'
 import AudioService from './services/AudioService.ts'
 import ConfettiService from './services/ConfettiService.ts'
+const BASE_URL = import.meta.env.BASE_URL
 
-const audioService = new AudioService('/sound/')
+const audioService = new AudioService(`${BASE_URL}/sound/`)
 const gameBuilder = new GameBuilder(new ImageLoaderService(), audioService, dataAnimals)
 
-const game = await gameBuilder
-  .loadSounds(soundsData)
-  .loadBackground(dataBackground)
-  .loadImageAnimals()
-  .build()
+;(async () => {
+  const game = await gameBuilder
+    .loadSounds(soundsData)
+    .loadBackground(dataBackground)
+    .loadImageAnimals()
+    .build()
 
-game.start()
+  game.start()
 
-document.querySelector('#restart')?.addEventListener('click', () => {
-  game.restart()
-})
+  document.querySelector('#restart')?.addEventListener('click', () => {
+    game.restart()
+  })
 
-document.querySelector('#mute')?.addEventListener('click', (e: MouseEvent) => {
-  const isMute = audioService.toggleSound()
-  const button = e.target as HTMLButtonElement
-  button.textContent = `Sound ${isMute ? '❌' : '🔉'}`
-})
+  document.querySelector('#mute')?.addEventListener('click', (e: Event) => {
+    const isMute = audioService.toggleSound()
+    const button = e.target as HTMLButtonElement
+    button.textContent = `Sound ${isMute ? '❌' : '🔉'}`
+  })
 
-const confettiService = new ConfettiService()
-game.onEndGame(() => {
-  setTimeout(() => confettiService.start(3), 0)
-})
+  const confettiService = new ConfettiService()
+  game.onEndGame(() => {
+    setTimeout(() => confettiService.start(3), 0)
+  })
+})()
